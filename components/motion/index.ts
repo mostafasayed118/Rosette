32cfd7dcf3c3a9d5676e55a98fa13619820996a0
Mustafa Sayed <mustafa_620125@oi.edu.eg>
2026-08-27 12:21:57 +0300

    feat(motion): add components/motion barrel export

---
+++ components/motion/index.ts
@@ -0,0 +1,7 @@
+export { MotionProvider } from "./MotionProvider";
+export { AnimatedButton } from "./AnimatedButton";
+export { Spinner } from "./Spinner";
+export { SpinnerOverlay } from "./SpinnerOverlay";
+export { MotionCard } from "./MotionCard";
+export { MotionSkeleton } from "./MotionSkeleton";
+export { PageTransition } from "./PageTransition";
